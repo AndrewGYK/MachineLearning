47bb04c096cdf1e54855f6a26a756dfef890efae
Andrew <andrew.gong@sap.com>
2019-04-22 17:52:22 +0800

    update

---
+++ GradientDescent/LinearRegressionWithOneVariable/computeCost.m
@@ -0,0 +1,9 @@
+function J = cost(X, y, theta)
+
+	m = length(y);
+
+	J = 0;
+
+	J = sum((X * theta - y) .^2) / (2*m);
+
+end
---
+++ GradientDescent/LinearRegressionWithOneVariable/gradientDescent.m
@@ -0,0 +1,22 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+
+	m = length(y);
+
+	J_history = zeros(num_iters, 1);
+
+	theta_s = theta;
+
+	for iter = 1:num_iters
+
+		theta(1) = theta(1) - (alpha / m) * sum(X * theta_s - y);
+
+		theta(2) = theta(2) - (alpha / m) * sum((X * theta_s - y) .* X(:,2));
+
+		theta_s = theta;
+
+
+		J_history(iter) = cost(X, y, theta);
+
+	end
+	
+end
