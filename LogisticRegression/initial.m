8fbc1bf8c89501d47f85f8cc796551071c9f79be
Andrew <andrew.gong@sap.com>
2019-04-30 13:43:36 +0800

    Update initial.m

--- LogisticRegression/initial.m
+++ LogisticRegression/initial.m
@@ -1,15 +1,11 @@
-function [X,y,theta,inter,alpha] = initial(data)
+function [X,y,theta] = initial(data)
+
+	[m,n] = size(data);
 	
 	y = data(:,3);
 
-	m = length(y);
+	X = [ones(m,1),data(:,[1,2]];
 
-	X = [ones(m,1),data(:,1),data(:,2)];
-
-	theta = [90;-1;-1];
-
-	inter = 1500;
-
-	alpha = 0.001;
+	theta = zeros(n,1);
 
 end
