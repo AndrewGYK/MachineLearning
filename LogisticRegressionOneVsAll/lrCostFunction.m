57b073f5b986721fc059163125adcdddb5f61acb
Andrew <andrew.gong@sap.com>
2019-05-14 16:04:28 +0800

    accomplish LrOneVSAll

--- LogisticRegressionOneVsAll/lrCostFunction.m
+++ LogisticRegressionOneVsAll/lrCostFunction.m
@@ -1,19 +1,18 @@
 function [J, grad] = lrCostFunction(theta, X, y, lambda)
  
 m = length(y);
-
 J = 0;
-
 grad = zeros(size(theta));
+h = sigmoid(X*theta);
 
-J = -1 * sum( y .* log( sigmoid(X*theta) ) + (1 - y ) .* log( (1 - sigmoid(X*theta)) ) ) / m ;
 
+J=(y'*log(h)+(1-y)'*log(1-h))/(-m);
+ 
 theta(1)=0;
  
 J+=theta'*theta*lambda/(2*m);
-
-grad=((X'*(sigmoid(X*theta)-y))+lambda*theta)/m;
-
+grad=((X'*(h-y))+lambda*theta)/m;
+ 
 grad = grad(:);
 
 end
